fa37d7b959b084b3ad502efe116304a9f3779234
Andrew Chen <andrew@jaanuu.com>
2026-01-19 23:13:21 -0800

    feat: Simplify OfflineTransfer component by removing unused state and handler

--- src/components/FileTransferPortal.tsx
+++ src/components/FileTransferPortal.tsx
@@ -1,4 +1,3 @@
-import { useState, useCallback } from 'react'
 import OfflineQRTransfer from './OfflineQRTransfer'
 
 interface OfflineTransferProps {
@@ -6,16 +5,10 @@ interface OfflineTransferProps {
 }
 
 export default function OfflineTransfer({ defaultMode = 'select' }: OfflineTransferProps) {
-  const [, setIsTransferActive] = useState(defaultMode !== 'select')
-
-  const handleModeChange = useCallback((mode: 'select' | 'send' | 'receive') => {
-    setIsTransferActive(mode !== 'select')
-  }, [])
-
   return (
     <div className="max-w-4xl mx-auto p-4">
       {/* Unified Transfer Interface */}
-      <OfflineQRTransfer defaultMode={defaultMode} onModeChange={handleModeChange} />
+      <OfflineQRTransfer defaultMode={defaultMode} />
     </div>
   )
 }
